7e55a2a1e47d315919eb8566a0945d91d1dc14dc
EmmaZou24 <emmabff7@gmail.com>
2025-12-30 14:28:45 -0500

    feat: base config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/emmazou24.github.io/', // Change this to your repository name, or '/' if using a custom domain
+  base: '/', // Root path for username.github.io repositories
 })
